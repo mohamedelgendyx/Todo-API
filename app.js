0e2374fd6037a8b0a302380c9e865ee6ad4d73ec
Mohamed Elgendy <mohamedelgendyx@gmail.com>
2023-06-27 03:01:50 +0300

    modify app file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+const app = express();
 /************************* Configrations *************************/
 require('dotenv').config();
 // only run all DB schemas
@@ -9,15 +10,21 @@ require('./models/todoModel');
 require('./models/userModel');
 
 const todoRouter = require('./routes/todoRoutes');
+const authenticationRouter = require('./routes/authenticationRoutes');
 
+const authenticationMW = require('./middlewares/authentcationMW');
 const notFoundMW = require('./middlewares/notFoundMW');
 const errorMW = require('./middlewares/errorMW');
 
+const seedMW = require('./middlewares/databaseSeedMW');
 const DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017/todo-db";
 const port = process.env.PORT || process.env.port || 3001;
 mongoose.connect(DBURI)
     .then(() => {
         console.log('Connected to MongoDB successfully');
+
+        // Apply the seed middleware
+        seedMW();
         // open a connection
         app.listen(port, () => {
             console.log(`server is listening on ${port}`);
@@ -25,11 +32,15 @@ mongoose.connect(DBURI)
     })
     .catch(error => console.log(`DB connection problem: ${error}`))
 
-const app = express();
 
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+// signin/signup
+app.use(authenticationRouter);
+// MW to check if there is a token and hook that token in the request
+app.use(authenticationMW);
 
 /*************************** Routes ***************************/
 app.use(todoRouter);
